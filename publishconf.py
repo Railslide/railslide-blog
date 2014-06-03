60c9cbdeb15d6a993767c148e47c5ab4205524d0
Giulia Vergottini <hello@railslide.io>
2014-06-04 00:53:33 +0200

    fix siteurl

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://railslide.github.io'
+SITEURL = 'http://railslide.github.io/railslide-blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
