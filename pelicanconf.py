a49e47911a872f5d3fd7c6c039e18a94aec456de
Giulia Vergottini <hello@railslide.io>
2014-06-08 18:34:21 +0200

    use static paths for CNAME

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,5 +30,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-# CNAME file
-FILES_TO_COPY = (('extra/CNAME', 'CNAME'),)
+# Static paths
+STATIC_PATHS = [
+    'CNAME',
+    'images'
+    ]
