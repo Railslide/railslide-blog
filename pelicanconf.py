f1da3e82bce5cd931f28aa0eaeae0b8073516a96
Giulia Vergottini <hello@railslide.io>
2014-06-08 18:19:42 +0200

    add CNAME

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# CNAME file
+FILES_TO_COPY = (('extra/CNAME', 'CNAME'),)
