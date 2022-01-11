6c1130952ac534a3ce0d80558f2212f3db693973
Giulia Vergottini <hello@railslide.io>
2022-01-11 14:31:46 +0100

    Linting

--- publishconf.py
+++ publishconf.py
@@ -7,14 +7,15 @@ from __future__ import unicode_literals
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://railslide.io'
+SITEURL = "https://railslide.io"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
